1faf093da8957db290f22ab155f48daa0630dced
Alexander Heinz <alexander.heinz@idealo.de>
2013-04-17 18:34:37 +0200

    alles mischt

--- app.js
+++ app.js
@@ -43,10 +43,32 @@ subscriber = JSON.parse( subscriber );
 console.log( subscriber );
 */
 
+var users = {
+	tehdb : {
+		'forename' : 'alex',
+		'lastname' : 'h'
+	},
+	murs : {
+		'forename' : 'evgeniya',
+		'lastname' : 'p'
+	}
+};
+
 app.get('/invite/:nick?', function(req, res){
 	//var nick = req.params.nick;
 	var nick = req.param('nick', 'noone');
-	res.render('invite', { title: nick });
+	//res.render('invite', { title: nick, user : users[nick] });
+	var user =  users[nick];
+
+	if( typeof user === 'undefined') {
+		res.status(302).redirect("/noone");
+	} else {
+		res.render('invite', { user : user, title : nick });
+	}
+});
+
+app.get('/noone', function(req, res){
+	res.render('noone', { title : 'досвидания' });
 });
 
 app.get('/red', function(req,res){
